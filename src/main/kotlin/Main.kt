6708b72e72cb10f0af0f5fefe08e06cd0c10e9c9
Beverlyakinyi <beverlyotienoakinyi@gmail.com>
2024-03-10 19:57:29 +0300

    Added Files

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,41 @@
+//Write a function named findLength that takes a String parameter and returns its length. However, make the return type nullable, indicating that it may return null if the input String is null.
+//
+fun main(){
+    var line=findlength("Beverly")
+println(line)
+}
+fun findlength(name:String):Int{
+    var x=name.length
+    return x
+
+
+
+
+
+//Write a function named average that takes a variable number of integers as its parameters (varargs) and returns the average of those numbers.
+
+fun main(){
+    println(Avarage(66,4))
+}
+fun Avarage(num1:Int,num2:Int):Int{
+    var a=num1 + num2
+    var b= a/2
+    return b
+
+
+//Create a Kotlin extension function named capitalizeEven for the String class. This function should capitalize every second character in the string. For example, if the input is "kotlin", the output should be "kOtLiN".
+//
+fun main(){
+println(capitalizeEven("Break"))
+}
+fun capitalizeEven(name:String):String{
+    var stringBulder=String()
+    for (B in "break"){
+        if (B 2 == 1){
+            StringBuilder("this(B.to Uppercase())")
+
+        }else{StringBuilder("this(B)"')')
+    }
+}
+    return StringBuilder
+    }
